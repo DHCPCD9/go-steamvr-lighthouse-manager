9991fa32fc7e529ff77e298194f71340505c00d3
DHCPCD <dhcpcd9@gmail.com>
2025-03-09 03:14:04 +0100

    fix: sizing

--- frontend/src/views/BaseStationView.jsx
+++ frontend/src/views/BaseStationView.jsx
@@ -24,11 +24,11 @@ export function BaseStationsList() {
 
             interval = setInterval(async () => {
                 let baseStations = await GetFoundBaseStations();
-                
+
                 setBaseStations(Object.values(baseStations));
             }, 300);
         })()
-        
+
         return () => clearInterval(interval);
     }, [searching])
     useEffect(() => {
@@ -45,44 +45,37 @@ export function BaseStationsList() {
 
     useEffect(() => {
         (async () => {
-            await smoothResize(700, 400, 150);
+            await smoothResize(700, 445, 150);
         })()
     }, []);
 
     return (<div className="flex flex-col gap-2 select-none">
-        <div className="text-white py-[12px] px-[24px] text-[24px] poppins-medium flex flex-row gap-[12px] items-center">
-            <ContainerTitleBar items={["Devices", activeBaseStation ? activeBaseStation : null]}/>
+        <div className="text-white py-[6px] px-[24px] text-[24px] poppins-medium flex flex-row gap-[12px] items-center">
+            <ContainerTitleBar items={["Devices", activeBaseStation ? activeBaseStation : null]} />
         </div>
         <div>
-                  {activeBaseStation && <AnimatePresence key={"base station"}>
-                    <motion.div key={"settings"} initial={{ x: -20, opacity: 0}} animate={{ x: 0, opacity: 1 }} exit={{ x: -20, opacity: 0 }}>
-
-                        <BaseStationSettings station={baseStations.find(c => c.Name == activeBaseStation)}/>
-                    </motion.div>
-
-</AnimatePresence>}
             <AnimatePresence>
-                
-                    {!activeBaseStation && <motion.div className="flex flex-col gap-[8px] px-[24px] py-[2px] max-height-[165px]" initial={{ opacity: 1 }} exit={{ x: -20, opacity: 0}}>
 
-                        {baseStations.map((station, index) =>
-                            <motion.div key={index} initial={{ opacity: 0, x: -30 }} animate={{ opacity: 1, x: 0 }} exit={{ opacity: 0, x: -20 }} transition={{ delay: (index + 1) * 0.150 }}>
-                                <BaseStation station={station} key={index + 1} setCurrentBaseStation={setActiveBaseStation} />
-                            </motion.div>
-                        )}
+                <div className="flex flex-col gap-[8px] px-[24px] py-[2px] max-h-[330px]">
 
-                        {searching ? <motion.div key={99} initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
-                            <div className="flex flex-row justify-center">
-                                <Loader2Icon className={"animate-spin"} color="white" />
-                            </div>
-                        </motion.div> : null}
-                        {!baseStations.length && !searching && <motion.div key={1337} initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} className="poppins-regular text-center text-[14px] text-[#C6C6C6]">
-                            No base stations seem to be found, maybe other programs are connected to them?
-                        </motion.div>}
+                    {baseStations.map((station, index) =>
+                        <motion.div key={index} initial={{ opacity: 0, x: -30 }} animate={{ opacity: 1, x: 0 }} exit={{ opacity: 0, x: -20 }} transition={{ delay: (index + 1) * 0.150 }}>
+                            <BaseStation station={station} key={index + 1} setCurrentBaseStation={setActiveBaseStation} />
+                        </motion.div>
+                    )}
+
+                    {searching ? <motion.div key={99} initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+                        <div className="flex flex-row justify-center">
+                            <Loader2Icon className={"animate-spin"} color="white" />
+                        </div>
+                    </motion.div> : null}
+                    {!baseStations.length && !searching && <motion.div key={1337} initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} className="poppins-regular text-center text-[14px] text-[#C6C6C6]">
+                        No base stations seem to be found, maybe other programs are connected to them?
                     </motion.div>}
-                    
-                </AnimatePresence>
-                
+                </div>
+
+            </AnimatePresence>
+
         </div>
     </div>)
 }
