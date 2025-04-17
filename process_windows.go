a704d5b8d111af9b8563e979249110b5f5716f14
DHCPCD <dhcpcd9@gmail.com>
2025-04-17 04:12:10 +0200

    chore: better implementation of process listing that doesn't have memory leak

--- process_windows.go
+++ process_windows.go
@@ -4,27 +4,39 @@
 package main
 
 import (
-	"bytes"
-	"os/exec"
-	"syscall"
+	"fmt"
+
+	"golang.org/x/sys/windows"
 )
 
-func isProcRunning(names ...string) (bool, error) { // I don't know if there is any better method to do it
-	if len(names) == 0 {
+const processEntrySize = 568
+
+func processID(name string) (uint32, error) {
+	h, e := windows.CreateToolhelp32Snapshot(windows.TH32CS_SNAPPROCESS, 0)
+	if e != nil {
+		return 0, e
+	}
+	p := windows.ProcessEntry32{Size: processEntrySize}
+	for {
+		e := windows.Process32Next(h, &p)
+		if e != nil {
+			break
+		}
+
+		if windows.UTF16ToString(p.ExeFile[:]) == name {
+			return p.ProcessID, nil
+		}
+	}
+	return 0, fmt.Errorf("%q not found", name)
+}
+
+func isProcRunning(name string) (bool, error) { // I don't know if there is any better method to do it
+
+	_, err := processID(name)
+
+	if err != nil {
 		return false, nil
 	}
 
-	cmd := exec.Command("tasklist.exe", "/fo", "csv", "/nh")
-	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
-	out, err := cmd.Output()
-	if err != nil {
-		return false, err
-	}
-
-	for _, name := range names {
-		if bytes.Contains(out, []byte(name)) {
-			return true, nil
-		}
-	}
-	return false, nil
+	return true, nil
 }
