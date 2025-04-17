5e4a55a3242a700d9a3c455e97d8d5127512ca01
DHCPCD <dhcpcd9@gmail.com>
2025-04-17 04:14:40 +0200

    chore: tell user when base station is in connecting state if it was cached.

--- frontend/src/components/BaseStation.jsx
+++ frontend/src/components/BaseStation.jsx
@@ -9,13 +9,14 @@ import { PowerStatusIcon } from "../assets/icons/PowerStatusIcon";
 import { route } from "preact-router";
 import { useTranslation } from "react-i18next";
 import { TitleBarSettingsIcon } from "../assets/icons/TitleBarSettingsIcon";
-export function BaseStation({ station, refreshBaseStations, onSelect, selected }) {
+import { ChevronRightIcon, CirclePower, Eye, Power, Settings } from "lucide-preact";
+export function BaseStation({ station, onSelect, selected }) {
 
     const isAwoke = [0x0B, 0x01, 0x09].includes(station.power_state);
 
-    const [identitfyDisabled, setIdentitfyhDisabled] = useState(false);    
+    const [identitfyDisabled, setIdentitfyhDisabled] = useState(false);
     const identitify = async () => {
-        
+
         let result = await IdentitifyBaseStation(station.id);
 
         if (result != "ok") return alert(result);
@@ -32,66 +33,75 @@ export function BaseStation({ station, refreshBaseStations, onSelect, selected }
             //Sleeping of
             let result = await ChangeBaseStationPowerStatus(station.id, "sleep");
             if (result != "ok") return alert(result);
-
-            return await refreshBaseStations();
+            return;
         }
 
         //Waking it up
         await ChangeBaseStationPowerStatus(station.id, "awake");
-        await refreshBaseStations();
     }
 
     const { t } = useTranslation();
 
     const baseStationStatus = useMemo(() => {
-        
+
         if (station.status == "error") return "error";
 
         if (station.status != "ready") return "preloaded"
 
         return isAwoke ? "awoke" : "sleep"
     }, [station.power_state, station.status])
-    
 
-    return (<div className={`text-white flex flex-row justify-between poppins-medium bg-[#1F1F1F] rounded-sm p-[16px] items-center hover:bg-[#434343] data-[selected="true"]:bg-[#434343] duration-200 cursor-pointer`} data-selected={selected}>
+
+    return (<div className={`text-white flex flex-row justify-between poppins-medium bg-[#1F1F1F] rounded-sm p-[16px] items-center hover:bg-[#434343] data-[selected="true"]:bg-[#434343] duration-200 cursor-pointer active:bg-[#1F1F1F]!`} data-selected={selected}>
         <div className="flex flex-row gap-[16px] items-center" onClick={() => {
             onSelect();
         }}>
-            <BaseStationIcon  />
+            <BaseStationIcon />
             <div className="flex flex-col gap-[2px] text-[14px]">
                 <span className="flex flex-row gap-[6px] items-center">
                     <span>{station.name} </span>
-                   <StatusCircleIcon class={`data-[status="sleep"]:fill-red-500 data-[status="preloaded"]:fill-blue-500 data-[status="awoke"]:fill-green-500 duration-300`}  data-status={baseStationStatus} />
+                    <StatusCircleIcon class={`data-[status="sleep"]:fill-red-500 data-[status="preloaded"]:fill-blue-500 data-[status="awoke"]:fill-green-500 duration-300`} data-status={baseStationStatus} />
 
                 </span>
-                <span className="text-[#C6C6C6]">
-                {station.version == 2 && <>{t("Channel")} {station.channel}</>} {station.version == 1 && <>lighthouse v{station.version} </>}
-                </span>
+                <AnimatePresence mode="wait">
+                    {station.status == "ready" && <motion.span className="text-[#C6C6C6]" initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+                        {t("Channel")} {station.channel}
+                    </motion.span>}
+
+                    {station.status != "ready" && <motion.span className="text-[#C6C6C6]" initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+                        Connecting...
+                    </motion.span>}
+
+
+                </AnimatePresence>
+
             </div>
         </div>
 
         <div className="flex flex-row gap-[8px] [&>*]:flex [&>*]:items-center">
             <AnimatePresence>
 
-            
-            {isAwoke && station.status == "ready" ? <motion.div key={"identitfy"} initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}}>
-                <button  className={"[&>svg]:fill-[#C6C6C6] opacity-75 hover:opacity-100 duration-150 disabled:opacity-25 scale-120 cursor-pointer"} onClick={identitify} disabled={identitfyDisabled}>
-                    <VisibilityIcon />
-                </button>
-            </motion.div>
-            : null}
 
-            <motion.div key={"awoke"}>
-                {station.status == "ready" && <button className="opacity-75 hover:opacity-100 duration-150 disabled:opacity-25 scale-120 cursor-pointer" onClick={updatePowerState}>
-                   <PowerStatusIcon class={`fill-[#C6C6C6]`} />
-                </button> }
-            </motion.div>
-            {/* <button key={"open"}>
+                {isAwoke && station.status == "ready" ? <motion.div key={"identitfy"} initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+                    <button className={"opacity-75 hover:opacity-100 duration-150 disabled:opacity-25 cursor-pointer p-1 border-[#C6C6C6] border-none rounded-md"} onClick={identitify} disabled={identitfyDisabled}>
+                        <Eye color="#C6C6C6" strokeWidth={2} />
+                    </button>
+                </motion.div>
+                    : null}
+
+                <motion.div key={"awoke"}>
+                    {station.status == "ready" && <button className="opacity-75 hover:opacity-100 duration-150 disabled:opacity-25 cursor-pointer p-1 border-[#C6C6C6] border-none rounded-md" onClick={updatePowerState}>
+                        <CirclePower color="#C6C6C6" strokeWidth={2} />
+                    </button>}
+                </motion.div>
+                {/* <button key={"open"}>
                 <ChevronRightIcon />
             </button> */}
-            <button key={"Settings"} className="opacity-75 hover:opacity-100 duration-150 disabled:opacity-25 scale-140 cursor-pointer" onClick={() => route(`/devices/${station.id}`)} >
-                <TitleBarSettingsIcon  class={`fill-[#C6C6C6]`}  />
-            </button>
+                <button key={"Settings"} className="opacity-75 hover:opacity-100 duration-150 disabled:opacity-25 cursor-pointer p-1 border-[#C6C6C6] border-none rounded-md" onClick={() => route(`/devices/${station.id}`)} >
+                    {/* <TitleBarSettingsIcon  class={`fill-[#C6C6C6]`}  /> */}
+                    <ChevronRightIcon />
+
+                </button>
             </AnimatePresence>
         </div>
     </div>)
