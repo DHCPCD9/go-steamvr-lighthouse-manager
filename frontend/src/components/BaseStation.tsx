ea0788da3a73e5a76316f68a4354423ed1f836bf
Wladyslaw Shinamo <dhcpcd9@gmail.com>
2025-08-25 17:24:12 +0200

    chore: make grouping look better

--- frontend/src/components/BaseStation.tsx
+++ frontend/src/components/BaseStation.tsx
@@ -11,11 +11,11 @@ import type { LighthouseStation } from "@src/lib/types/index";
 import { useWebsocketCommunication } from "@src/lib/hooks/useWebsocketCommunication";
 
 
-export function BaseStation({ station, onSelect, selected }: { station: LighthouseStation, onSelect?: () => void, selected: boolean }) {
+export function BaseStation({ station, onSelect, selected, editMode }: { station: LighthouseStation, onSelect?: () => void, selected: boolean, editMode?: boolean }) {
 
     const isAwoke = [0x0B, 0x01, 0x09].includes(station.power_state);
     const send = useWebsocketCommunication();
- 
+
     const [identitfyDisabled, setIdentitfyhDisabled] = useState(false);
     const identitify = async () => {
 
@@ -52,10 +52,10 @@ export function BaseStation({ station, onSelect, selected }: { station: Lighthou
         return isAwoke ? "awoke" : "sleep"
     }, [station.power_state, station.status])
 
-    return (<div className={`text-white flex flex-row justify-between poppins-medium bg-[#1F1F1F] rounded-sm p-[16px] items-center hover:bg-[#434343] data-[selected="true"]:bg-[#434343] duration-200 cursor-pointer active:bg-[#1F1F1F]!`} data-selected={selected}>
-        <div className="flex flex-row gap-[16px] items-center" onClick={() => {
-            if (onSelect) onSelect();
-        }}>
+    return (<div className={`text-white flex flex-row justify-between poppins-medium bg-[#1F1F1F] rounded-sm p-[16px] items-center hover:bg-[#434343] data-[selected="true"]:bg-[#434343] duration-200 cursor-pointer active:bg-[#1F1F1F]!`} data-selected={selected} onClick={() => {
+        if (onSelect && editMode) onSelect();
+    }}>
+        <div className="flex flex-row gap-[16px] items-center">
             <BaseStationIcon />
             <div className="flex flex-col gap-[2px] text-[14px]">
                 <span className="flex flex-row gap-[6px] items-center">
@@ -82,26 +82,25 @@ export function BaseStation({ station, onSelect, selected }: { station: Lighthou
             <AnimatePresence>
 
 
-                {isAwoke && station.status == "ready" ? <motion.div key={"identitfy"} initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+                {isAwoke && station.status == "ready" && !editMode ? <motion.div key={"identitfy"} initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
                     <button className={"opacity-75 hover:opacity-100 duration-150 disabled:opacity-25 cursor-pointer p-1 border-[#C6C6C6] border-none rounded-md"} onClick={identitify} disabled={identitfyDisabled}>
                         <Eye color="#C6C6C6" strokeWidth={2} />
                     </button>
                 </motion.div>
                     : null}
 
-                <motion.div key={"awoke"}>
+                {!editMode && <motion.div key={"awoke"} initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
                     {station.status == "ready" && <button className="opacity-75 hover:opacity-100 duration-150 disabled:opacity-25 cursor-pointer p-1 border-[#C6C6C6] border-none rounded-md" onClick={updatePowerState}>
                         <CirclePower color="#C6C6C6" strokeWidth={2} />
                     </button>}
-                </motion.div>
-                {/* <button key={"open"}>
-                <ChevronRightIcon />
-            </button> */}
-                <button key={"Settings"} className="opacity-75 hover:opacity-100 duration-150 disabled:opacity-25 cursor-pointer p-1 border-[#C6C6C6] border-none rounded-md" onClick={() => route(`/devices/${station.id}`)} >
-                    {/* <TitleBarSettingsIcon  class={`fill-[#C6C6C6]`}  /> */}
-                    <ChevronRightIcon />
+                </motion.div>}
 
-                </button>
+                {!editMode && <motion.div key={"open"} initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+                    <button key={"Settings"} className="opacity-75 hover:opacity-100 duration-150 disabled:opacity-25 cursor-pointer p-1 border-[#C6C6C6] border-none rounded-md" onClick={() => route(`/devices/${station.id}`)} >
+                        <ChevronRightIcon />
+                    </button>
+                </motion.div>}
+
             </AnimatePresence>
         </div>
     </div>)
--- frontend/src/views/BaseStationView.tsx
+++ frontend/src/views/BaseStationView.tsx
@@ -1,7 +1,7 @@
 import { BaseStation } from "../components/BaseStation";
 import { useContext, useEffect, useState } from 'preact/hooks'
 import { motion, AnimatePresence } from 'framer-motion'
-import { Copy, Loader, Loader2Icon, RefreshCcw } from "lucide-preact";
+import { Copy, Loader, Loader2Icon, PencilIcon, PencilOffIcon, PlusIcon, RefreshCcw } from "lucide-preact";
 import { AddBaseStationToGroup, CreateGroup, GetConfiguration, InitBluetooth } from "@src/lib/native/index";
 import { smoothResize } from "../utils/windows";
 import { ContainerTitleBar } from "../components/ContainerTitleBar";
@@ -17,7 +17,7 @@ import { WebsocketContext } from "@src/lib/context/websocket.context";
 import { StartScanFor10Seconds } from "../../wailsjs/go/main/App";
 export function BaseStationsList() {
 
-    
+
     const baseStations = useLighthouses();
     const groups = useLighthouseGroups();
     const config = useConfig();
@@ -26,13 +26,15 @@ export function BaseStationsList() {
     const [, push] = useRouter();
     const [selectedBaseStations, setSelectedBaseStation] = useState([]);
     const { setItems, setCallbackOnLast } = useContainerTitlebar();
+    const [editModeEnabled, setEditModeEnabled] = useState(false);
+    
 
     const { t } = useTranslation();
 
     useEffect(() => {
         (async () => {
             await InitBluetooth();
-            await smoothResize(700, 450);
+            await smoothResize(700, 460);
         })()
     }, []);
 
@@ -45,10 +47,11 @@ export function BaseStationsList() {
 
     const groupBaseStations = async () => {
 
+        if (!selectedBaseStations.length) return;
         let newName = "New Group"
         let i = 0;
 
-        
+
         for (const group of Object.keys(config.groups)) {
             if (group == newName + (i > 0 ? `(${i + 1})` : "")) {
                 i++;
@@ -63,6 +66,11 @@ export function BaseStationsList() {
         return push(`/groups/${group}`);
     }
 
+    const toggleEditMode = async () => {
+        setEditModeEnabled(!editModeEnabled);
+        setSelectedBaseStation([]);
+    }
+
     useEffect(() => {
         if (selectedBaseStations.length > 0) {
             setCallbackOnLast(() => {
@@ -72,39 +80,67 @@ export function BaseStationsList() {
         }
 
         setItems([{ text: "Devices", link: "/" }])
-    }, [])
+    }, []);
 
 
     return (<div className="flex flex-col gap-2 select-none justify-between">
         <div>
             <AnimatePresence>
                 <div className="flex flex-col gap-[8px] px-[24px] py-[2px] h-[335px] overflow-y-auto" key={"base stations"} style={{ scrollbarWidth: 'none' }}>
-                {Object.entries(groups).filter(([k,v]) => v).map(([k, v]) => (<BaseStationGroup key={`${k}-group`} id={k} group={v} />))}
+                    {Object.entries(groups).filter(([k, v]) => v).map(([k, v]) => (<BaseStationGroup key={`${k}-group`} id={k} group={v} />))}
 
                     {baseStations.map((station, index) =>
                         <motion.div key={index} initial={{ opacity: 0, x: -30 }} animate={{ opacity: 1, x: 0 }} exit={{ opacity: 0, x: -20 }} transition={{ delay: (index + 1) * 0.150 }}>
-                            <BaseStation station={station} key={index + 1} selected={selectedBaseStations.includes(station.id)} onSelect={() => selectBaseStation(station.id)} />
+                            <BaseStation station={station} key={index + 1} selected={selectedBaseStations.includes(station.id)} onSelect={() => editModeEnabled && selectBaseStation(station.id)} editMode={editModeEnabled} />
                         </motion.div>
                     )}
 
-                    {<motion.div key={99} initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
-                        <div className="flex flex-row justify-center" onClick={() => StartScanFor10Seconds()}>
-                            <RefreshCcw data-disabled={scanning} className={`text-white/80 duration-200 hover:text-white active:text-white/80 data-[disabled="false"]:cursor-pointer data-[disabled="true"]:text-white/40 data-[disabled="true"]:animate-spin`}/>
-                        </div>
-                    </motion.div>}
+
                     {!baseStations.length && !scanning && <motion.div key={1337} initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} className="poppins-regular text-center text-[14px] text-[#C6C6C6]">
                         {t("No base stations seem to be found, maybe other programs are connected to them?")}
                     </motion.div>}
                 </div>
 
 
-            
-            {selectedBaseStations.length > 0 && baseStations.length > 0 && <motion.div className="px-[24px] z-10" initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ ease: "linear"}} >
-                <button  className="bg-[#1F1F1F] text-[#C6C6C6] poppins-regular w-full py-[4px] text-[14px] rounded-[6px] hover:bg-[#434343] duration-200 cursor-pointer active:bg-[#0D0D0D]!" onClick={() => groupBaseStations()}>
-                    Create group
-                </button>
-            </motion.div>}
-            
+                <div class={"flex flex-row justify-end px-[24px] pb-[12px] gap-2"}>
+                    <AnimatePresence>
+                        {!editModeEnabled && <motion.div key={99} initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+                            <div className="flex flex-row justify-center clickable rounded-md" onClick={() => StartScanFor10Seconds()}>
+                                <RefreshCcw size={24} data-disabled={scanning} className={`text-white/80 duration-200 hover:text-white active:text-white/80 data-[disabled="false"]:cursor-pointer data-[disabled="true"]:text-white/40 data-[disabled="true"]:animate-spin`} />
+                            </div>
+                        </motion.div>}
+
+                        {toggleEditMode && <div className={`flex flex-row justify-center clickable rounded-md data-[disabled="true"]:opacity-50 data-[disabled="true"]:cursor-not-allowed`} data-disabled={selectedBaseStations.length < 1} onClick={() => groupBaseStations()}>
+                            <PlusIcon size={24} data-disabled={scanning} className={`text-white/80 duration-200 hover:text-white active:text-white/80 data-[disabled="false"]:cursor-pointer data-[disabled="true"]:text-white/40 data-[disabled="true"]:animate-spin`} />
+                        </div>}
+
+                        <div  data-active={editModeEnabled} className={`flex flex-row justify-center clickable rounded-md data-[active="true"]:bg-[#1D81FF]`} onClick={() => toggleEditMode()}>
+                            <AnimatePresence mode="popLayout">
+
+                                {!editModeEnabled ?
+                                    <motion.div key={"edit"} initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0, rotate: -180 }} transition={{ type: "tween", easings: ["linear"]}} >
+                                        <PencilIcon size={24} data-active={editModeEnabled} className={`text-white/80 duration-200 hover:text-white active:text-white/80 data-[disabled="false"]:cursor-pointer data-[disabled="true"]:text-white/40 data-[disabled="true"]:animate-spin`} /> 
+                                    </motion.div> :
+                                     <motion.div key={"close"} initial={{ opacity: 0, rotate: -180 }} animate={{ opacity: 1, rotate: -365 }} exit={{ opacity: 0 }} transition={{ type: "tween", easings: ["linear"]}}>
+                                        <PencilOffIcon size={24} className={`text-white/80 duration-200 hover:text-white active:text-white/80 data-[disabled="false"]:cursor-pointer data-[disabled="true"]:text-white/40 data-[disabled="true"]:animate-spin`} /> 
+                                    </motion.div>}
+
+                            </AnimatePresence>
+                        </div>
+
+
+                        {/* {editModeEnabled && <motion.div className="px-[24px] z-10" initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ ease: "linear" }} >
+                        <button className="bg-[#1F1F1F] text-[#C6C6C6] poppins-regular w-full py-[4px] text-[14px] rounded-[6px] hover:bg-[#434343] duration-200 cursor-pointer active:bg-[#0D0D0D]! disabled:opacity-50 disabled:cursor-not-allowed" disabled={selectBaseStation.length < 1} onClick={() => groupBaseStations()}>
+                            Create group
+                        </button>
+                    </motion.div>} */}
+                    </AnimatePresence>
+                </div>
+
+
+
+
+
             </AnimatePresence>
 
         </div>
